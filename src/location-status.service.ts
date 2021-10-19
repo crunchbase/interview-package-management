95b034081e98e38f8897e0a9964c342463422dda
Amanda <amanda@crunchbase.com>
2021-10-19 12:20:45 -0700

    Locations for package 123 instead of 456

--- src/location-status.service.ts
+++ src/location-status.service.ts
@@ -16,19 +16,19 @@ export class StoredLocationStatus {
 const storedLocationStatuses: StoredLocationStatus[] = [
   {
     id: 1011,
-    packageId: 456,
+    packageId: 123,
     address: '1 Package Dropoff Way, Oakland, CA, 11111',
     status: LocationStatusEnum.ARRIVED,
   },
   {
     id: 1213,
-    packageId: 456,
+    packageId: 123,
     address: '1 Package Dropoff Way, Oakland, CA, 11111',
     status: LocationStatusEnum.DEPARTED,
   },
   {
     id: 1415,
-    packageId: 456,
+    packageId: 123,
     address: '4 Midway Street, Columbus, OH, 44444',
     status: LocationStatusEnum.ARRIVED,
   },
