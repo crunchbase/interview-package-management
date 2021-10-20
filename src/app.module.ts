e7ec0d26e147c09b8073adb4e8a62c2bef5f767d
Amanda <amanda@crunchbase.com>
2021-10-20 11:23:35 -0700

    Include PackagesWithTrackingController

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { PackagesController } from './packages.controller';
 import { LocationStatusService } from './location-status.service';
 import { PackageService } from './package.service';
 import { AppController } from './app.controller';
+import { PackagesWithTrackingController } from './packages-with-tracking.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, PackagesController],
+  controllers: [AppController, PackagesController, PackagesWithTrackingController],
   providers: [LocationStatusService, PackageService],
 })
 export class AppModule {}
